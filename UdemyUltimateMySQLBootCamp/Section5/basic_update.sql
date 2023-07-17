bb569b0926d630d5ed52942a93237c92f4f0150f
Eric Ricketts <eric_ricketts@me.com>
2023-07-17 19:09:48 -0400

    finish Section 5 examples and exercises

---
+++ UdemyUltimateMySQLBootCamp/Section5/basic_update.sql
@@ -0,0 +1,6 @@
+-- CODE: Updating Data
+-- Change tabby cats to shorthair:
+UPDATE cats SET breed='Shorthair' WHERE breed='Tabby';
+
+-- Another update:
+UPDATE cats SET age=14 WHERE name='Misty';
---
+++ UdemyUltimateMySQLBootCamp/Section5/update_exercise.sql
@@ -0,0 +1,15 @@
+-- run the SELECT before the UPDATE to make sure UPDATE
+-- does what you desire
+SELECT * FROM cats WHERE name='Jackson';
+UPDATE cats SET name='Jack' WHERE name='Jackson';
+SELECT * FROM cats WHERE name='Jackson';
+SELECT * FROM cats WHERE name='Jack';
+
+SELECT * FROM cats WHERE name='Ringo';
+UPDATE cats SET breed='British Shorthair' WHERE name='Ringo';
+SELECT * FROM cats WHERE name='Ringo';
+SELECT * FROM cats;
+
+SELECT * FROM cats WHERE breed='Maine Coon';
+UPDATE cats SET age=12 WHERE breed='Maine Coon';
+SELECT * FROM cats WHERE breed='Maine Coon';
