2a6bd923675db86c280b33155f9f649e1945d4e5
Eric Ricketts <eric_ricketts@me.com>
2023-07-20 06:38:33 -0400

    Section 7 other string functions

---
+++ UdemyUltimateMySQLBootCamp/Section7/other_string_functions.sql
@@ -0,0 +1,10 @@
+SELECT INSERT('Hello Bobby', 6, 0, ' There');
+SELECT INSERT('Hello Bobby', 6, 4, ' There');
+SELECT INSERT('Hello Bobby', 6, 6, ' There');
+SELECT RIGHT('omghahalol!', 4);
+SELECT LEFT('omghahalol!', 3);
+SELECT REPEAT('ha', 4);
+SELECT TRIM ('   pickle   ');
+SELECT TRIM(LEADING '.' FROM '....San Antonio...');
+SELECT TRIM(TRAILING '.' FROM '....San Antonio...');
+SELECT TRIM(BOTH '.' FROM '....San Antonio...');
