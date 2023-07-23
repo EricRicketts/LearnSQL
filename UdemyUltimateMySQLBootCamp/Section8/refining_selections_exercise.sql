b29dcea68c6ef807b6b3f8f9aeef7fc0dd73bc1d
Eric Ricketts <eric_ricketts@me.com>
2023-07-23 08:48:18 -0400

    Section 8 exercise on refining SELECTS

--- UdemyUltimateMySQLBootCamp/Section8/refining_selections_exercise.sql
+++ UdemyUltimateMySQLBootCamp/Section8/refining_selections_exercise.sql
@@ -1,19 +1,32 @@
+-- Exercise 1
 SELECT title
 FROM books WHERE title LIKE '%Stories%';
+
+-- Exercise 2
 SELECT title, pages
 FROM books
 ORDER BY pages DESC LIMIT 1;
+
+-- Exercise 3
 SELECT CONCAT(title, ' - ', released_year) AS summary
 FROM books ORDER BY released_year DESC LIMIT 3;
+
+-- Exercise 4
 SELECT title, author_lname
 FROM books
 WHERE author_lname LIKE '% %';
+
+-- Exercise 5
 SELECT title, released_year, stock_quantity
 FROM books
 ORDER BY stock_quantity LIMIT 3;
+
+-- Exercise 6
 SELECT title, author_lname
 FROM books
 ORDER BY author_lname, title;
+
+-- Exercise 7
 SELECT CONCAT(
     UPPER('my favorite author is '),
     UPPER(author_fname),
@@ -21,5 +34,18 @@ SELECT CONCAT(
     UPPER(author_lname),
     '!'
     ) AS yell
+FROM books
+ORDER BY author_lname;
+
+-- Exercise 7 simpler answer
+SELECT UPPER(
+               CONCAT(
+                       'my favorite author is ',
+                       author_fname,
+                       ' ',
+                       author_lname,
+                       '!'
+                   )
+           ) AS yell
 FROM books
 ORDER BY author_lname;
