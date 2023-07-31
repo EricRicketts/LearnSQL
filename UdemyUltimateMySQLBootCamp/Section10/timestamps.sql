93a047a05181e53455337ec2c4fe28b7d74b7ff6
Eric Ricketts <eric_ricketts@me.com>
2023-07-31 18:48:49 -0400

    Section 10 update timestamps.sql

--- UdemyUltimateMySQLBootCamp/Section10/timestamps.sql
+++ UdemyUltimateMySQLBootCamp/Section10/timestamps.sql
@@ -1,7 +1,7 @@
 CREATE TABLE captions (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    text VARCHAR(225),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    text VARCHAR(225) DEFAULT '' NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 INSERT INTO captions (text)
@@ -9,10 +9,14 @@ VALUES ('this is the first entry'),
        ('and this is the second entry');
 
 ALTER TABLE captions ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        ON UPDATE CURRENT_TIMESTAMP;
+        ON UPDATE CURRENT_TIMESTAMP NOT NULL;
 
 UPDATE captions
 SET text = 'this is the first entry updated'
 WHERE id = 1;
 
 SELECT * FROM captions;
+
+ALTER TABLE captions MODIFY COLUMN text VARCHAR(255) DEFAULT '' NOT NULL;
+ALTER TABLE captions MODIFY COLUMN updated_at TIMESTAMP NOT NULL;
+ALTER TABLE captions MODIFY COLUMN created_at TIMESTAMP NOT NULL;
