4236c543092dce32c2d28a24a22188e4e05e39f6
Eric Ricketts <eric_ricketts@me.com>
2023-08-10 05:54:03 -0400

    Section 11 logical operators OR

--- UdemyUltimateMySQLBootCamp/Section11/logical_operators.sql
+++ UdemyUltimateMySQLBootCamp/Section11/logical_operators.sql
@@ -28,3 +28,19 @@ FROM books
 WHERE
     CHAR_LENGTH(title) > 15
     AND pages > 500;
+
+SELECT
+    title,
+    author_lname,
+    released_year
+FROM books
+WHERE
+    author_lname = 'Eggers'
+    OR released_year > 2010;
+
+SELECT
+    title, pages
+FROM books
+WHERE
+    pages < 200
+    OR title LIKE '%stories%';
