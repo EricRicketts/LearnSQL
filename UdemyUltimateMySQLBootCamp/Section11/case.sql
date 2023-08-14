b65520a9e35aeda324ec3f8c657456d11434b957
Eric Ricketts <eric_ricketts@me.com>
2023-08-14 07:09:32 -0400

    Section 11 case statements in mySQL

---
+++ UdemyUltimateMySQLBootCamp/Section11/case.sql
@@ -0,0 +1,27 @@
+SELECT * FROM books;
+
+SELECT
+    author_lname, author_fname, title, released_year,
+CASE
+    WHEN released_year >= 2000 THEN 'Modern Literature'
+    ELSE '20th Century Literature'
+END AS 'Genre'
+FROM books;
+
+SELECT
+    title, stock_quantity,
+    CASE
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS stock
+FROM books;
+
+SELECT
+    title, stock_quantity,
+    CASE
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+        END AS stock
+FROM books;
--- UdemyUltimateMySQLBootCamp/Section11/logical_operators.sql
+++ UdemyUltimateMySQLBootCamp/Section11/logical_operators.sql
@@ -44,3 +44,95 @@ FROM books
 WHERE
     pages < 200
     OR title LIKE '%stories%';
+
+SELECT
+    title, released_year
+FROM books
+WHERE released_year >= 2004 AND released_year <= 2015;
+
+SELECT
+    title, released_year
+FROM books
+WHERE released_year BETWEEN 2004 AND 2015;
+
+SELECT
+    title, released_year
+FROM books
+WHERE released_year NOT BETWEEN 2004 AND 2015;
+
+SELECT
+    title, pages
+FROM books
+WHERE pages BETWEEN 200 AND 300;
+
+SELECT
+    title, pages
+FROM books
+WHERE pages NOT BETWEEN 200 AND 300;
+
+SELECT *
+FROM people
+WHERE BirthDate < '1950-01-01';
+
+-- this is more explicit and readable than the above solution
+SELECT *
+FROM people
+WHERE YEAR(BirthDate) < 2005;
+
+SELECT *
+FROM people
+WHERE BirthTime > '09:00:00';
+
+SELECT *
+FROM people
+WHERE HOUR(BirthTime) > 9;
+
+-- this is the technically correct version of comparing times if you want
+-- more precision that just hours because of you just compare hours then
+-- you will get results on the same hour but which also includes minutes and
+-- seconds AFTER the hour.  In the format below we can compare in a way which
+-- includes minutes and seconds.  Note mySQL can understand a string version
+-- of time, 'hh:mm:ss' but the more robust way is to CAST the string to a TIME
+-- object.
+SELECT *
+FROM people
+WHERE
+    BirthTime
+        BETWEEN CAST('05:00:00' AS TIME) AND CAST('08:00:00' AS TIME);
+
+-- as mentioned above this syntax below, while more readable, does not take into
+-- account minutes and seconds after the hour, so if the developer wants to restrict
+-- the comparison to 08:00:00 and any time before this will not work.
+SELECT *
+FROM people
+WHERE
+    HOUR(BirthTime) BETWEEN 5 AND 8;
+
+SELECT * FROM books;
+
+SELECT
+    title, author_lname, author_fname
+FROM books
+WHERE author_lname IN ('Saunders', 'Lahiri');
+
+SELECT
+    title, author_lname, author_fname
+FROM books
+WHERE author_lname NOT IN ('Saunders', 'Lahiri');
+
+SELECT
+    title, author_lname, author_fname, released_year
+FROM books
+WHERE books.released_year NOT IN (2000, 2001, 2002, 2003, 2004);
+
+-- get all even year released books
+SELECT
+    title, author_lname, author_fname, released_year
+FROM books
+WHERE released_year % 2 = 0;
+
+-- get all odd year released books
+SELECT
+    title, author_lname, author_fname, released_year
+FROM books
+WHERE released_year % 2 = 1;
