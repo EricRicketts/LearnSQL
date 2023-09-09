76fae21acd28735d513fa20b172efbe59804535b
Eric Ricketts <eric_ricketts@me.com>
2023-09-09 14:36:12 -0400

    finish unique window functions

--- UdemyUltimateMySQLBootCamp/Section16/unique_window_fuctions.sql
+++ UdemyUltimateMySQLBootCamp/Section16/unique_window_fuctions.sql
@@ -42,3 +42,64 @@ SELECT
     ROW_NUMBER() OVER(ORDER BY salary DESC) AS overall_row_number
 FROM more_employees ORDER BY department;
 
+SELECT
+    emp_no,
+    department,
+    salary,
+    NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
+FROM more_employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile,
+    NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
+FROM more_employees ORDER BY department;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) AS high_pay_dept_emp_no,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) AS high_pay_emp_no
+FROM more_employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    LAG(salary) OVER(ORDER BY salary DESC) AS lagging_salary
+FROM more_employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    LAG(salary, 2) OVER(ORDER BY salary DESC) AS lagging_salary
+    -- specifies 2 rows back
+FROM more_employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    LEAD(salary) OVER(ORDER BY salary DESC) AS leading_salary
+FROM more_employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    salary - LEAD(salary) OVER(PARTITION BY department ORDER BY salary DESC)
+        AS dept_salary_diffs
+FROM more_employees;
+
+SELECT
+    emp_no,
+    department,
+    salary,
+    -salary + LAG(salary) OVER(PARTITION BY department ORDER BY salary DESC)
+        AS dept_salary_diffs
+FROM more_employees;
+
