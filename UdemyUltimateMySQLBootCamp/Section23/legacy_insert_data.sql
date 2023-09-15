9d68a1f2e3290b5cc1febdd353eec1bde833ff58
Eric Ricketts <eric_ricketts@me.com>
2023-09-15 07:55:00 -0400

    Legacy insert data into table section 23

---
+++ UdemyUltimateMySQLBootCamp/Section23/legacy_insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO legacy_cats(name, breed, age)
+VALUES
+    ('Asher', 'Abyssinian', 1),
+    ('Abe', 'American Bobtail', 2),
+    ('Albert', 'American Curl', 3),
+    ('Angie', 'American Short Hair', 4),
+    ('Aretha', 'American Wire Hair', 1),
+    ('Aaron', 'Balinese', 2),
+    ('Andrew', 'Bengal', 3),
+     ('Abigail', 'Birman', 4);
+SELECT * FROM legacy_cats;
