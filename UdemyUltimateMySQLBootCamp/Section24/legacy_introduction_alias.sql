d309d81265d618ee565afdd60767a9f25f173dc2
Eric Ricketts <eric_ricketts@me.com>
2023-09-22 04:42:38 -0400

    legacy select introductoin section 24

---
+++ UdemyUltimateMySQLBootCamp/Section24/legacy_introduction_alias.sql
@@ -0,0 +1,13 @@
+SELECT
+    cat_id AS id
+FROM cats;
+
+SELECT
+    name AS 'cat name',
+    breed AS 'cat breed'
+FROM cats;
+
+SELECT
+    name AS cat_name,
+    breed AS cat_breed
+FROM cats;
---
+++ UdemyUltimateMySQLBootCamp/Section24/legacy_select_introduction.sql
@@ -0,0 +1,9 @@
+SELECT * FROM cats;
+SELECT name, age FROM cats;
+SELECT name, breed FROM cats;
+
+SELECT * FROM cats
+WHERE age = 4;
+
+SELECT * FROM cats
+WHERE name = 'Egg';
